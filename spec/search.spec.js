79bbf3186cc8aaf7c4c9e7b7994b452456f290d9
Vitor Alano <alano.vitor@gmail.com>
2015-03-18 07:35:42 -0300

    Adding tests and depth search.

--- spec/search.spec.js
+++ spec/search.spec.js
@@ -35,4 +35,19 @@ describe("SearchEngine", function () {
     
   });
   
+  describe("when calling the limited depth first search", function () {
+    
+    eightPuzzle.puzzle.state = [[null, 5, 2], [1, 4, 3], [7, 8, 6]];
+    eightPuzzle.puzzle.empty = { x: 0, y: 0 };
+    search.limitedDepthFirst(25);
+    
+    it("should organize the puzzle", function () {
+      
+      expect(eightPuzzle.isEndState()).to.be.equals(true);
+      
+    });
+    
+  });
+  
+  
 });
